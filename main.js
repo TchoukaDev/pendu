bb986c5279ba5ccc2563be3137fe88abfb98b2a3
Romain Wirth <romain.wirth@gmail.com>
2025-01-28 23:32:04 +0100

    second commit

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const mots = ["console", "manette", "zelda", "mario", "switch", "playstation", "
 let nombreAleatoire;
 const regles            = $("#regles");
 const formulaire        = $("form");
+let motCache
+let motEpele;
 let lettreSaisie;
 let motSaisi;
-let tentativesRestantes = 10;
-
+let indexMot = -1;
+let tentativesRestantes;
 
 formulaire.submit((event)=> {
     event.preventDefault()
@@ -29,41 +31,42 @@ formulaire.submit((event)=> {
         regles.css("display", "block");
         finRegles.css("display", "block");
         finRegles.click(() => {
-          afficherJeu();
-          genererJeu();
+        genererJeu();
         }
         )
     }
 })
 
-function afficherJeu() {
+function genererJeu() {
     bienvenue.remove()
     regles.css("display", "none");
     finRegles.remove();
     $("#inputSaisies").css("display", "block");      
     $("#pendu").css("display", "block");
-    afficherMasquerRegles();   
+    afficherMasquerRegles();
+    verifierSaisie();
+    genererMotCache(mots);
+    creerChangerMot();   
 }
 
-function genererJeu() {
-    erreurSaisie();
-    genererMot(mots);
-    changerMot();
-
-}
-
-function genererMot(tableau) {
+function genererMotCache(tableau) {
     tentativesRestantes = 10;
     genererNombreAleatoire(mots.length);
-    const motEpele = [...tableau[nombreAleatoire]]
+    motCache = tableau[nombreAleatoire].toUpperCase();
+    console.log(motCache)
+    motEpele = [...motCache];
+    console.log(motEpele);
 
-    motEpele.forEach ((lettre) => {
-        const span = $("<span></span>")
+    motEpele.forEach ((lettre, index) => {
+        const span   = $("<span></span>")
         span.text(lettre)
         span.addClass("hidden");
         $("#motCache").append(span);
+        span.attr("id", `span${lettre}${index}`)
         }
     )
+    afficherTentatives(tentativesRestantes);
+    etatPendu(tentativesRestantes);
 }
 
 function afficherMasquerRegles() {
@@ -84,36 +87,123 @@ function afficherMasquerRegles() {
     })
 }
 
-function changerMot() {
+function creerChangerMot() {
     const nouveauMot = $("<button>Changer de mot</button>");
     $("#changerMot").append(nouveauMot);
     nouveauMot.click(() => {
         genererNombreAleatoire();
         $("span").remove();
-        genererMot(mots)
+        genererMotCache(mots);
     })
-
-
 }
 
-function erreurSaisie() {
-    const erreurSaisie = $("#erreurSaisie");
 
-        $("#validerLettre, #validerMot").click(() => {
-            motSaisi     = $("#saisirMot").val().trim();
-            lettreSaisie = $("#saisirLettre").val().trim();
+function verifierSaisie() {
+     $("#validerLettre, #validerMot").click(() => {
 
-            if (lettreSaisie === "" && motSaisi === "") {
+        erreurSaisie = $("#erreurSaisie")
+        motSaisi     = $("#saisirMot").val().trim();
+        lettreSaisie = $("#saisirLettre").val().trim();
+
+        if (lettreSaisie === "" && motSaisi === "") {
         erreurSaisie.text("Veuillez saisir une lettre ou un mot");
         erreurSaisie.css("color", "red");
-            }
-            else {
-            erreurSaisie.text("");
-         }
-        })
+        }
+        else {
+        erreurSaisie.text("");
+        saisirTentatives();
+        $("#saisirLettre").val("");
+        $("#saisirMot").val("");
+        }
+    })
+}
+
+function saisirTentatives() {
+        if (lettreSaisie === "é" || lettreSaisie === "è" || lettreSaisie === "ê" || lettreSaisie === "ë") {
+            lettreSaisie = "e";
+        }
+        else if (lettreSaisie === "à" || lettreSaisie === "â" || lettreSaisie === "ä") {
+            lettreSaisie = "a";
+        } 
+        else if (lettreSaisie === "ô" || lettreSaisie === "ö") {
+            lettreSaisie = "o";
+        }
+        else if (lettreSaisie === "î" || lettreSaisie === "ï") {
+            lettreSaisie = "i";
+        }
+        else if (lettreSaisie === "û" || lettreSaisie === "ù" || lettreSaisie === "ü") {
+            lettreSaisie = "u";
+        }
+        else if (lettreSaisie === "ç") {
+            lettreSaisie = "c";
+        }
+        lettreSaisie = lettreSaisie.toUpperCase();
+        
+        let positionsLettre = []
+
+    motEpele.forEach((lettre, index) => {
+        if (lettre === lettreSaisie) {
+            positionsLettre.push(index);
+        }
+    })
+
+        if (positionsLettre.length > 0) {
+            positionsLettre.forEach((position) => {
+            let lettreDevoilee = $(`#span${lettreSaisie}${position}`);
+            lettreDevoilee.css("color", "black");
+            })
+        }
+        else {
+            tentativesRestantes --;
+            afficherTentatives(tentativesRestantes);
+            etatPendu(tentativesRestantes);
+        }
+
+    $("#validerMot").click(() => {
+        motSaisi = motSaisi.toUpperCase();
+
+        if (motSaisi === motCache) {
+            // gagnerPartie();
+        }
+        else {
+        tentativesRestantes --
+        afficherTentatives(tentativesRestantes);
+        etatPendu(tentativesRestantes);
+        }
+    
+    })
      }
 
-     function genererNombreAleatoire(max) {
+function afficherTentatives(nombre) {
+    const idTentativesRestantes = $("#tentativesRestantes");
+    idTentativesRestantes.text(`Tentatives restantes: ${nombre}`)
+    if (tentativesRestantes >= 8) {
+        idTentativesRestantes.css("color", "green");        
+    }
+    else if (tentativesRestantes === 6 || tentativesRestantes === 7) {
+        idTentativesRestantes.css("color", "lightgreen");
+    }
+    else if (tentativesRestantes <= 5 && tentativesRestantes >= 3) {
+        idTentativesRestantes.css("color", "orange");
+    }
+    else if (tentativesRestantes === 1 || tentativesRestantes === 2) {
+        idTentativesRestantes.css("color", "red");
+    }
+    else {
+        // PartiePerdue();
+    }
+}
+
+function etatPendu(nombre) {
+    $("#imagePendu").attr("src", `images/pendu${nombre}.jpg`);
+    console.log(`Changement de l'image : pendu${nombre}.jpg`);
+}
+
+function genererNombreAleatoire(max) {
+        do {
         nombreAleatoire = Math.floor(Math.random() * Math.floor(max))
+        }
+        while (nombreAleatoire === indexMot)
+        indexMot = nombreAleatoire;
         return nombreAleatoire
      }    
